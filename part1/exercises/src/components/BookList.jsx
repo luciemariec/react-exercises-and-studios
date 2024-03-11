e6dc8474793c5a721f923dbdf1459b8135b2fe1c
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-03-11 15:55:05 -0500

    react part one updated

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,16 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Read This Month";
+   let book1 = "One Day";
+   let book2 = "The Ballad of Songbirds and Snakes";
+   let book3 = "Because of Miss Bridgerton";
 
    return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+   <div>
+<h3>{pageTitle}</h3>
+<img src={"https://m.media-amazon.com/images/I/411rEm4QjxL._SY445_SX342_.jpg"} alt="One Day by David Nicholls"/>
+<img src={"https://m.media-amazon.com/images/I/51qoASMxDzL._SY445_SX342_.jpg"} alt="The Ballad of Songbirds and Snakes by Suzanne Collins"/>
+<img src={"https://m.media-amazon.com/images/I/51TMj2v1rnL._SY445_SX342_.jpg"} alt="Because of Miss Bridgerton by Julia Quinn" />
+</div>
+   
+);
 }
